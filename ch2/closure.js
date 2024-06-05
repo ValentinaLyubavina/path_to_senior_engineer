91a12a23ee230b90647f1d20eea23ebbb981d85e
Valentina Lyubavina <valentinalyubavina@MacBook-Valentina.local>
2024-06-05 15:26:01 +0300

    Fix

--- ch2/closure.js
+++ ch2/closure.js
@@ -21,3 +21,27 @@ function inBetween(arr) {
 }
 
 console.log(inBetween(arr));
+
+let arr2 = [1, 2, 3, 4, 5, 6, 7];
+
+function inArray(arr) {
+  return function (item) {
+    return arr.includes(item);
+  };
+}
+
+console.log(arr2.filter(inArray([1, 2, 10])));
+
+let users = [
+  { name: "Иван", age: 20, surname: "Иванов" },
+  { name: "Пётр", age: 18, surname: "Петров" },
+  { name: "Анна", age: 19, surname: "Каренина" },
+];
+
+function byField(key) {
+  return function sortKey(a, b) {
+    return a[key] > b[key] ? 1 : -1;
+  };
+}
+
+console.log(users.sort(byField("name")));
